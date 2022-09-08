0a31dffb0540192e5db8a94bc5801b7c040bc246
danisheurus <danish.hafeez@eurustechnologies.com>
2022-09-08 23:05:31 +0500

    Slack Shared Library

---
+++ Sharedlibs/slack.groovy
@@ -0,0 +1,12 @@
+def COLOR_MAP = [
+    'SUCCESS': 'good',
+    'ABORTED': 'warning',
+    'FAILURE': 'danger'
+]
+
+def call(result){
+    slackSend channel: 'danishjenkinstest',
+                    color: COLOR_MAP[currentBuild.currentResult],
+                    message: "Status : *${currentBuild.currentResult}:* \n Job name: ${env.JOB_NAME} build no. : ${env.BUILD_NUMBER} \n\n For more Information: ${env.BUILD_URL}"
+}
+
